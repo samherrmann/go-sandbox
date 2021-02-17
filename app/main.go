248af2f15294f0d0930993b70b5d34c64f185922
Sam Herrmann <samherrmann.github@gmail.com>
2021-02-17 12:07:37 -0500

    Serve files from embedded filesystem

--- app/main.go
+++ app/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "log"
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+//go:embed static
+var embeddedFS embed.FS
 
 func main() {
-	log.Println("Hello World!")
+	serverRoot, err := fs.Sub(embeddedFS, "static")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/", http.FileServer(http.FS(serverRoot)))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ app/static/main.js
@@ -0,0 +1,1 @@
+document.querySelector('h1').innerHTML = 'Hello Go 1.16!'
