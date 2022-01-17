66482d02a9bcd3bcb6b9e2f928f30d591d05d639
Sam Herrmann <samherrmann.github@gmail.com>
2022-01-16 20:05:30 -0500

    Change working directory to config file location

--- app/main.go
+++ app/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	log.Println("Hello World!")
+	configPath := "path/to/config.json"
+
+	// Change working directory to the directory in which the config file is
+	// located. That way all relative paths within the config file will be
+	// evaluated correctly.
+	if err := os.Chdir(filepath.Dir(configPath)); err != nil {
+		panic(err)
+	}
+
+	bytes, err := os.ReadFile(filepath.Base(configPath))
+	if err != nil {
+		panic(err)
+	}
+
+	config := &Config{}
+	if err := json.Unmarshal(bytes, config); err != nil {
+		panic(err)
+	}
+
+	bytes, err = os.ReadFile(config.Path)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(bytes))
+}
+
+type Config struct {
+	Path string `json:"path"`
 }
