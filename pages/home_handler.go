f3767170abadc201921baaee58c0482e777ec86a
Sam Herrmann <samherrmann.github@gmail.com>
2024-03-11 13:58:41 +0000

    Add NotFound to home handler

--- pages/home_handler.go
+++ pages/home_handler.go
@@ -3,5 +3,12 @@ package pages
 import "net/http"
 
 func NewHomeHandler(redirectPaths string) http.Handler {
-	return http.RedirectHandler(redirectPaths, http.StatusMovedPermanently)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Process exact match only.
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		http.Redirect(w, r, redirectPaths, http.StatusMovedPermanently)
+	})
 }
