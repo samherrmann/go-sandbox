abb1ea1b11de464ac5fc9b8eeb626beda4837d66
Sam Herrmann <samherrmann.github@gmail.com>
2025-06-27 21:38:42 -0400

    Send query with variables

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,47 +19,66 @@ func main() {
 
 func app() error {
 
+	if len(os.Args) < 2 {
+		return fmt.Errorf("missing order number")
+	}
+
+	orderNumber := os.Args[1]
+	if !strings.HasPrefix(orderNumber, "#") {
+		orderNumber = "#" + orderNumber
+	}
+
 	config, err := readConfig("config.json")
 	if err != nil {
 		return err
 	}
 
-	query := `{ __type(name: "Order") { fields { name } } }`
+	query, err := os.ReadFile("query.graphql")
+	if err != nil {
+		return err
+	}
 
-	res, err := sendGraphQL(config.Shop, config.AccessToken, query)
+	req := &GraphQLRequest{
+		Query: string(query),
+		Variables: map[string]string{
+			"name": orderNumber,
+		},
+	}
+
+	res, err := sendGraphQL(config.Shop, config.AccessToken, req)
 	if err != nil {
 		return fmt.Errorf("sending GraphQL request: %w", err)
 	}
 
-	body, err := io.ReadAll(res)
-	if err != nil {
+	if _, err := io.Copy(os.Stdout, res); err != nil {
 		return fmt.Errorf("reading response: %w", err)
 	}
 	defer res.Close()
 
-	fmt.Println(string(body))
-
 	return nil
 }
 
-func sendGraphQL(shop string, accessToken string, query string) (io.ReadCloser, error) {
+func sendGraphQL(shop string, accessToken string, req *GraphQLRequest) (io.ReadCloser, error) {
 	url := fmt.Sprintf(
 		"https://%s.myshopify.com/admin/api/unstable/graphql.json",
 		shop,
 	)
 
-	payload := fmt.Sprintf(`{"query": %q}`, query)
+	payload, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payload)))
+	httpReq, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
 	if err != nil {
 		return nil, fmt.Errorf("new request: %w", err)
 	}
 
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("X-Shopify-Access-Token", accessToken)
+	httpReq.Header.Set("Content-Type", "application/json")
+	httpReq.Header.Set("X-Shopify-Access-Token", accessToken)
 
 	client := &http.Client{}
-	res, err := client.Do(req)
+	res, err := client.Do(httpReq)
 	if err != nil {
 		return nil, fmt.Errorf("send HTTP request: %w", err)
 	}
@@ -67,4 +88,9 @@ func sendGraphQL(shop string, accessToken string, query string) (io.ReadCloser, 
 	}
 
 	return res.Body, nil
+}
+
+type GraphQLRequest struct {
+	Query     string `json:"query"`
+	Variables any    `json:"variables"`
 }
